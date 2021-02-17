860b63b59144c10c93417cad79a438f6b1def402
SEETHAMRAJU <spurvaj@gmail.com>
2021-02-17 12:06:14 +0530

    Added few other functionality

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,12 +3,24 @@ public class Calculator {
     Calculator(String name){
         this.name = name;
     }
-    public int add(int x,int y){
-        return x+y;
-    }
+
     public double sqrt_f(double x){
         double answer = Math.sqrt(x*1.0);
         return answer;
     }
+//    Logarithm base natural
+//    power function
+    public int factorial(int x){
+        int fact = 1;
+        if(x<0)
+            return 0;
+        if(x<=1)
+            return fact;
+        for(int i=2;i<=x;i++)
+            fact*=i;
+        return fact;
+    }
+
+
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,18 +5,21 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     @Test
-    public void add() {
-        Calculator c = new Calculator("my_calc");
-        int x = c.add(1,2);
-        assertEquals(3,x);
-    }
-
-    @Test
     public void sqrt_test() {
         Calculator c = new Calculator("my_calc");
         double x = c.sqrt_f(4);
         assertEquals(2.0,x,0.0);
         x = c.sqrt_f(5);
         assertEquals(2.23606797749979,x,0.005);
+    }
+    @Test
+    public void factorial_test() {
+        Calculator c = new Calculator("my_calc");
+        int x = c.factorial(4);
+        assertEquals(24,x);
+        x = c.factorial(0);
+        assertEquals(1,x);
+        x = c.factorial(-2);
+        assertEquals(0,x);
     }
 }
