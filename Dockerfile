a30ae888c6f2eb70fbdcb898ff6a37d0e9da512b
SEETHAMRAJU <spurvaj@gmail.com>
2021-03-06 21:50:41 +0530

    Added logging

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,10 @@ RUN mvn clean install
 
 FROM openjdk:8-jre
 
-COPY --from=maven_builder /build/target/calculator-1.0-SNAPSHOT.jar .
+COPY --from=maven_builder /build/target/calculator-1.0-SNAPSHOT-jar-with-dependencies.jar .
 COPY input.txt .
 
 #We can either use CMD or ENTRYPOINT. But for the sake of trying all possible commands
 
-CMD java -cp calculator-1.0-SNAPSHOT.jar:/build/target/classes/ Driver < ./input.txt
-
-#ENTRYPOINT ["java","-cp" ,"calculator-1.0-SNAPSHOT.jar:/build/target/classes", "Driver"]
+CMD java -jar calculator-1.0-SNAPSHOT-jar-with-dependencies.jar < ./input.txt
+#ENTRYPOINT ["java","-jar" ,"calculator-1.0-SNAPSHOT.jar", "Driver"]
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,23 +1,37 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import org.apache.log4j.Property;
 public class Calculator {
     String name;
+    private final static Logger logger = LogManager.getLogger();
+
     Calculator(String name){
         this.name = name;
+        logger.info("[CALCULATOR " + name + " CREATED]");
     }
 
     public double sqrt_f(double x){
-        double answer = Math.sqrt(x*1.0);
+        logger.info("[SQRT_FN] : "+x);
+        double answer = Math.sqrt(x);
+        logger.info("[SQRT_FN - RESULT] : "+answer);
         return answer;
     }
 //    Logarithm base natural
 //    power function
     public int factorial(int x){
         int fact = 1;
-        if(x<0)
+        logger.info("[FACTORIAL] : "+x);
+        if(x<0){
+            logger.info("[FACTORIAL - RESULT] : 0");
             return 0;
-        if(x<=1)
+        }
+        if(x<=1) {
+            logger.info("[FACTORIAL - RESULT] : "+fact);
             return fact;
+        }
         for(int i=2;i<=x;i++)
             fact*=i;
+        logger.info("[FACTORIAL - RESULT] : "+fact);
         return fact;
     }
 }
